596a6be934fc8a60f0f73c8bfa3f0a48d8062017
Prakhar Pandey <prakharpandeylfs@gmail.com>
2024-02-05 14:49:53 +0530

    first commit

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -3,11 +3,11 @@ import NewsItem from "./NewsItem";
 
 const NewsBoard = ({category}) => {
     const [articles,setArticles]=useState([]);
-
+    
     useEffect(()=>{
-        let url=`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
-        fetch(url).then(response=>response.json()).then(data=>setArticles(data.articles));
+        let url=`https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=in&max=10&apikey=${import.meta.env.VITE_API_KEY}`;
 
+        fetch(url).then(response=>response.json()).then(data=>setArticles(data.articles));
     },[category])
 
   return (
